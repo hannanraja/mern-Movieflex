e4d6f362514f8740b99f2becf9f94cea457e1d37
MuhammmadHannan <hannanraja1992@gmail.com>
2022-09-22 16:11:08 +0500

    backend

---
+++ client/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = new express();
+const router = require('./router.js')
+const port = 4300;
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
+}));
+app.listen(port, () => {
+    console.log("Port is listeneing at port :" + port)
+})
+
+app.get('/', (req, res) => {
+    res.status(200).send(
+` <h1>Get the data from user and display it</h1>`
+    )
+})
+app.use('/operation', router)
